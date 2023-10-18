ffb8c5f225db96d1a6ae73b8f9079acb18e5ab61
SalsabilaYulianti <141704344+SalsabilaYulianti@users.noreply.github.com>
2023-10-18 07:22:24 +0700

    Add files via upload

---
+++ Belanja/belanja3.js
@@ -0,0 +1,15 @@
+let belanja = 100
+
+if(belanja < 100){
+    console.log("tidak dapat diskon")
+}
+
+else if(belanja == 100) {
+    console.log("Selamat kamu mendapatkan diskon 50%")
+        
+}
+else{
+    console.log("Selamat kamu mendapatkan diskon 10%")
+        
+}
+    
